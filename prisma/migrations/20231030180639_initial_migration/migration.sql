6cec222942b9ef3c470db2f815ad13b129b775ca
Rodolpheanger <rodolphe.anger@yahoo.fr>
2023-10-31 12:00:47 +0100

    🖥️ wip: first commit

---
+++ prisma/migrations/20231030180639_initial_migration/migration.sql
@@ -0,0 +1,16 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('USER', 'STAFF', 'ADMIN');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "role" "Role" NOT NULL DEFAULT 'USER',
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
